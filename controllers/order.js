2e119cfa7ac0498f6edf14302bdf5048441f2bf4
oksususu <eclipse.peachch@gmail.com>
2024-03-07 22:17:01 +0900

    주문 API 내 DB 트랜잭션 적용

--- controllers/order.js
+++ controllers/order.js
@@ -3,14 +3,14 @@ const client = require("../config/dbClient");
 const Order = require("../models/order");
 const OrderItem = require("../models/orderItem");
 const Product = require("../models/product");
-
-const user_check_query = "select * from public.user where user_id =";
+const sequelize = require("../config/orm");
 
 /* 주문 조회 */
 exports.getOrder = async (req, res) => {
   const { user_id: userId } = req.user;
   let orderDataList = [];
 
+  const order_limit = 5;
   const orders = await Order.findAll({ where: { user_id: userId } });
   for (let order of orders) {
     let { order_id: orderId, total_price: totalPrice, order_date: orderDate, status } = order;
@@ -18,7 +18,7 @@ exports.getOrder = async (req, res) => {
 
     const brandId2OrderItems = {};
     // const orderItems = await OrderItem.findAll({ where: { order_id: orderId }, include: [{ model: Product, include: [{ model: Brand }] }] });
-    const orderItems = await OrderItem.findAll({ where: { order_id: orderId }, include: [{ model: Product }] });
+    const orderItems = await OrderItem.findAll({ where: { order_id: orderId }, limit: order_limit, include: [{ model: Product }] });
     for (let orderItem of orderItems) {
       let brandId = orderItem.Product.brand_id;
       if (Object.keys(brandId2OrderItems).includes(brandId)) {
@@ -38,10 +38,20 @@ exports.getOrder = async (req, res) => {
 exports.createOrder = async (req, res) => {
   const { userId, totalPrice, orderItems } = req.body;
 
-  const order = await Order.create({ user_id: userId, total_price: totalPrice });
-  const orderId = order.order_id;
-  for (let orderItem of orderItems) {
-    await OrderItem.create({ order_id: orderId, product_id: orderItem.productId, quantity: orderItem.quantity, price: orderItem.price });
+  // 참고문서: https://sequelize.org/docs/v6/other-topics/transactions/
+  try {
+    await sequelize.transaction(async (t) => {
+      const order = await Order.create({ user_id: userId, total_price: totalPrice }, { transaction: t });
+      const orderId = order.order_id;
+      for (let orderItem of orderItems) {
+        await OrderItem.create(
+          { order_id: orderId, product_id: orderItem.productId, quantity: orderItem.quantity, price: orderItem.price },
+          { transaction: t }
+        );
+      }
+    });
+  } catch (error) {
+    throw new Error("주문 생성 중 에러 발생으로, 주문 생성 cancel" + error);
   }
 
   res.json("ok");
@@ -52,12 +62,18 @@ exports.cancelOrder = async (req, res) => {
   const { id: orderId } = req.params;
 
   // sequelize
-  const orderToUpdate = await Order.findByPk(orderId);
-  if (!orderToUpdate) {
-    throw new ValidationError(404, "존재하지 않는 주문 id입니다");
+  try {
+    await sequelize.transaction(async (t) => {
+      const orderToUpdate = await Order.findByPk(orderId);
+      if (!orderToUpdate) {
+        throw new ValidationError(404, "존재하지 않는 주문 id입니다");
+      }
+      orderToUpdate.status = -1;
+      await orderToUpdate.save({ transaction: t });
+    });
+  } catch (error) {
+    throw new Error("주문 취소 중 에러 발생으로, 주문 취소 cancel " + error);
   }
-  orderToUpdate.status = -1;
-  await orderToUpdate.save();
 
   //raw sql
   // const query = `update order set status=-1 where order_id = ${orderId}`;
